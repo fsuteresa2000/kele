1f0a4111a2bf69ff0d474f4b3daf7a496b2b7d27
fsuteresa2000 <fsuteresa2000@yahoo.com>
2017-08-30 10:13:03 -0500

    checkpoint-4-mentor-availability-complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -10,18 +10,20 @@ class Kele
   end
 
   def initialize(email, password)
-    options = {
-      body: { email: email, password: password }
-    }
-    response = self.class.post("#{base_uri}/sessions", options)
-    @auth_token = response['auth_token']
-    if @auth_token.nil?
+      response = self.class.post( "/sessions", body: {"email": email, "password": password } )
+      @auth_token = response["auth_token"]
+      if @auth_token.nil?
       puts "Sorry, invalid credentials."
     end
   end
 
   def get_me
-    response = self.class.get("#{base_uri}/users/me", headers: { "authorization" => @auth_token })
-    JSON.parse(response.body)
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    @mentor_availability_data = JSON.parse(response.body)
   end
 end
