1505af7c00a94c1dafb353f8f478c83f45b25bb6
fsuteresa2000 <fsuteresa2000@yahoo.com>
2017-08-30 13:06:37 -0500

    checkpoint-6-messaging-complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -29,3 +29,26 @@ class Kele
     @mentor_availability_data = JSON.parse(response.body)
   end
 end
+
+def get_messages(page=nil)
+    if page == nil
+       response = self.class.get("/message_threads", headers: { "authorization" => @auth_token })
+    else
+       response = self.class.get("/message_threads?page=#{page}", headers: { "authorization" => @auth_token })
+    end
+     @messages_data = JSON.parse(response.body)
+  end
+
+  def create_message(user_email, recipient_id, subject, message)
+    response = self.class.post("/messages",
+      body: {
+        "sender": user_email,
+        "recipient_id": recipient_id,
+        "subject": subject,
+        "stripped_text": message
+      },
+      headers: {"authorization" => @auth_token})
+    puts response
+  end
+
+end
